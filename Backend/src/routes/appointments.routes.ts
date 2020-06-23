c4f174e0e637ea3fd682d5f07c0edcc74c860ae6
Alquipo <alquiponeto@outlook.com.br>
2020-06-23 10:32:33 -0300

    chore: validação da data agendamento

--- Backend/src/routes/appointments.routes.ts
+++ Backend/src/routes/appointments.routes.ts
@@ -1,17 +1,39 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  // CONVERTE PARA DATA E INICIA COM HORA ZERADA
+  const parsedDate = startOfHour(parseISO(date));
+
+  // PERCORRE O ARRAY DE APPOINTMENT PARA COMPARAR AS DATAS
+  const findAppoitmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  // SE A DATA JA ESTIVER MARCADA RETORNA O ERRO
+  if (findAppoitmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
