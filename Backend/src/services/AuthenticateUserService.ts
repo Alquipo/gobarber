640fda4087177dee31b1a921ce89de3abe602623
Alquipo <alquiponeto@outlook.com.br>
2020-06-24 16:28:56 -0300

    chore: token authenticate

--- Backend/src/services/AuthenticateUserService.ts
+++ Backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -32,9 +34,14 @@ class AuthenticateUserService {
     }
 
     // usuario autenticado
+    const token = sign({}, '34340f6187c62fdecd780f840ab20dcb', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
 
     return {
       user,
+      token,
     };
   }
 }
