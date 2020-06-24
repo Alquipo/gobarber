bb07bb5d5d1eb09330ef54bd5b91e6b2643eda21
Alquipo <alquiponeto@outlook.com.br>
2020-06-24 20:28:25 -0300

    chore: update avatar

---
+++ Backend/src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,42 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import uploadConfig from '../config/upload';
+import User from '../models/User';
+
+interface Request {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const usesrRepository = getRepository(User);
+
+    const user = await usesrRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar');
+    }
+
+    if (user.avatar) {
+      // deletar avatar anterior
+
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usesrRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
