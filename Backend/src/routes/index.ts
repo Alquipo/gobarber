21fd353603281f1acc9c128163888561e1eb6252
Alquipo <alquiponeto@outlook.com.br>
2020-06-24 15:28:20 -0300

    feat: user table

--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -1,9 +1,12 @@
 // src/routes/index.ts
 import { Router } from 'express';
+
 import appointmentRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
---
+++ Backend/src/routes/users.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    return response.send();
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default usersRouter;
