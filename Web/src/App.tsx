c60fb2d5b6deb2ef73584db8be2ca00b4ee56a5f
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-03 19:05:48 -0300

    feat: context API

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
+import AuthContext from './context/AuthContext';
+
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'Alquipo' }}>
+      <SignIn />
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
