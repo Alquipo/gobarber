829f9ff845fdb75fa63066d63916c4a349684d2c
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-05 13:40:14 -0300

    feat: creation Toast

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
-import { AuthProvider } from './context/AuthContext';
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
 
 import GlobalStyle from './styles/global';
 
@@ -12,6 +13,8 @@ const App: React.FC = () => (
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>
