6905ae7e0c52d0f26073c960e857f35ff28cf294
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-05 20:48:18 -0300

    feat: unform add

--- Mobile/src/pages/SignUp/index.tsx
+++ Mobile/src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {
   Image,
   View,
@@ -8,6 +8,8 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -17,6 +19,8 @@ import logoImg from '../../assets/logo.png';
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const navigation = useNavigation();
 
   return (
@@ -33,23 +37,21 @@ const SignUp: React.FC = () => {
           <Container>
             <Image source={logoImg} />
 
-            <View>
-              <Title>Crie sua conta</Title>
-            </View>
+            <Form ref={formRef} onSubmit={() => {}}>
+              <View>
+                <Title>Crie sua conta</Title>
+              </View>
 
-            <Input name="name" icon="user" placeholder="Nome" />
+              <Input name="name" icon="user" placeholder="Nome" />
 
-            <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="email" icon="mail" placeholder="E-mail" />
 
-            <Input name="password" icon="lock" placeholder="Senha" />
+              <Input name="password" icon="lock" placeholder="Senha" />
 
-            <Button
-              onPress={() => {
-                console.log('deu');
-              }}
-            >
-              Entrar
-            </Button>
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Entrar
+              </Button>
+            </Form>
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
