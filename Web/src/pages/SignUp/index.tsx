6e77aa929754b073b74b122e3a3938436bac31dd
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-05 16:36:48 -0300

    fix: timeout Toast

--- Web/src/pages/SignUp/index.tsx
+++ Web/src/pages/SignUp/index.tsx
@@ -48,13 +48,13 @@ const SignUp: React.FC = () => {
 
         await api.post('/users', data);
 
-        history.push('/');
-
         addToast({
           type: 'success',
           title: 'Cadastro realizado!',
           description: 'Você já pode fazer seu logon no GoBarber!',
         });
+
+        history.push('/');
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err);
