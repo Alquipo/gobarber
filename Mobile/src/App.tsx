a3eae75d60b964b553da29a768dd4de8d6e465cb
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-05 22:27:15 -0300

    chore: autenticação

--- Mobile/src/App.tsx
+++ Mobile/src/App.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+import AppProvider from './hooks';
 import Routes from './routes';
 
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar barStyle="default" backgroundColor="#312e38" />
-    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-      <Routes />
-    </View>
+    <AppProvider>
+      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+        <Routes />
+      </View>
+    </AppProvider>
   </NavigationContainer>
 );
 
