307b2d6784cd1f8d35e8eb99fe7d43da50f6b614
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-30 11:41:36 -0300

    feat: TDD Forgot Password

---
+++ Backend/src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const SendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await fakeUsersRepository.create({
+      name: 'fulano',
+      email: 'fulando@example.com',
+      password: '123456',
+    });
+
+    await SendForgotPasswordEmail.execute({
+      email: 'fulando@example.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});
