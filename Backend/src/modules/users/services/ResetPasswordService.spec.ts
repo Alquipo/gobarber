3a8ff83c1b5dcbc5547b4abce63d56a1958ccd20
Alquipo Neto <alquiponeto@outlook.com.br>
2020-07-30 19:13:37 -0300

    fix

--- Backend/src/modules/users/services/ResetPasswordService.spec.ts
+++ Backend/src/modules/users/services/ResetPasswordService.spec.ts
@@ -66,4 +66,27 @@ describe('resetPasswordService', () => {
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
+
+  it('should be able to reset password if passed more than 2 hours', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'fulano',
+      email: 'fulando@example.com',
+      password: '123456',
+    });
+
+    const { token } = await fakeUserTokensRepository.generate(user.id);
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      const customDate = new Date();
+
+      return customDate.setHours(customDate.getHours() + 3);
+    });
+
+    await expect(
+      resetPassword.execute({
+        password: '123123',
+        token,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
