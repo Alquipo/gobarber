944745e4635e4bc44b17de4434b93518bf5f6c65
Alquipo <alquiponeto@outlook.com.br>
2020-08-09 08:50:42 -0300

    feat: redis config

--- Backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ Backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,5 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentRepository from '../repositories/IAppointmentsRepository';
 
@@ -15,6 +16,9 @@ class ListProviderAppointmentsService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -23,6 +27,9 @@ class ListProviderAppointmentsService {
     month,
     day,
   }: IRequest): Promise<Appointment[]> {
+    const cacheData = await this.cacheProvider.recover('asdasd');
+    console.log(cacheData);
+
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
       {
         provider_id,
@@ -31,7 +38,7 @@ class ListProviderAppointmentsService {
         day,
       },
     );
-
+    // await this.cacheProvider.save('asdasd', 'saasfd');
     return appointments;
   }
 }
--- Backend/src/shared/container/providers/index.ts
+++ Backend/src/shared/container/providers/index.ts
@@ -3,3 +3,5 @@ import './StorageProvider';
 import './MailTemplateProvider';
 
 import './MailProvider';
+
+import './CacheProvider';
