5f0998fcc6c60e0d3b028877c1d46a6e455218aa
Alquipo <alquiponeto@outlook.com.br>
2020-08-10 16:08:04 -0300

    chore: styles header createAppointmens

--- Mobile/src/pages/CreateAppointment/index.tsx
+++ Mobile/src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,45 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { useAuth } from '../../hooks/auth';
+
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+} from './styles';
+
+interface RouteParams {
+  providerId: string;
+}
 const CreateAppointment: React.FC = () => {
-  return <View />;
+  const { user } = useAuth();
+
+  const { goBack } = useNavigation();
+
+  const route = useRoute();
+
+  const { providerId } = route.params as RouteParams;
+
+  const navigateBack = useCallback(() => {
+    goBack();
+  }, [goBack]);
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+    </Container>
+  );
 };
 
 export default CreateAppointment;
