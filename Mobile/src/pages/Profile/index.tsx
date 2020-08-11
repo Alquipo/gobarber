4ef1d2f99ecebd311df2d67a9693e93d810eae72
Alquipo <alquiponeto@outlook.com.br>
2020-08-11 19:01:30 -0300

    feat: image-picker

--- Mobile/src/pages/Profile/index.tsx
+++ Mobile/src/pages/Profile/index.tsx
@@ -12,6 +12,8 @@ import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import ImagePicker from 'react-native-image-picker';
+
 import Icon from 'react-native-vector-icons/Feather';
 
 import api from '../../services/api';
@@ -122,6 +124,38 @@ const SignUp: React.FC = () => {
     [navigation, updateUser],
   );
 
+  const handleUpdateAvatar = useCallback(() => {
+    ImagePicker.showImagePicker(
+      {
+        title: 'Selecione um avatar',
+        cancelButtonTitle: 'Cancelar',
+        takePhotoButtonTitle: 'Usar câmera',
+        chooseFromLibraryButtonTitle: 'Escolher da galeria',
+      },
+      response => {
+        if (response.didCancel) {
+          return;
+        }
+
+        if (response.error) {
+          Alert.alert('Erro ao atualizar seu avatar');
+        }
+
+        const data = new FormData();
+
+        data.append('avatar', {
+          type: 'image/jpeg',
+          name: `${user.id}.jpg`,
+          uri: response.uri,
+        });
+
+        api.patch('users/avatar', data).then(apiResponse => {
+          updateUser(apiResponse.data);
+        });
+      },
+    );
+  }, [updateUser, user.id]);
+
   const handleGoBack = useCallback(() => {
     navigation.goBack();
   }, [navigation]);
@@ -142,7 +176,7 @@ const SignUp: React.FC = () => {
               <Icon name="chevron-left" size={21} color="#999591" />
             </BackButton>
 
-            <UserAvatarButton onPress={() => {}}>
+            <UserAvatarButton onPress={handleUpdateAvatar}>
               <UserAvatar source={{ uri: user.avatar_url }} />
             </UserAvatarButton>
 
