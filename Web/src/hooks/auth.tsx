5f607e23185ff1ddf1899310cf4308e91abd6e13
Alquipo <alquiponeto@outlook.com.br>
2020-08-12 15:23:45 -0300

    feat: Auth test

--- Web/src/hooks/auth.tsx
+++ Web/src/hooks/auth.tsx
@@ -85,9 +85,9 @@ export const AuthProvider: React.FC = ({ children }) => {
 export function useAuth(): AuthContextData {
   const context = useContext(AuthContext);
 
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
+  // if (!context) {
+  //   throw new Error('useAuth must be used within an AuthProvider');
+  // }
 
   return context;
 }
